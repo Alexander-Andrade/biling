6eb90f1eb02b3aac4376df3295ff3e7139750650
Alexander Andrade <a.andrade@sumatosoft.com>
2018-12-23 16:13:19 +0300

    google translate

--- biling.rb
+++ biling.rb
@@ -3,12 +3,19 @@ require 'json'
 require 'nokogiri'
 
 
-def translate(text)
+def yandex_translate(text)
   text = text.gsub(/[^[:ascii:]]/, '')
   key = 'trnsl.1.1.20181222T190255Z.5f681e8011285a14.931279157d16121692edf18e8ad5665d98b55e84'
   uri = URI("https://translate.yandex.net/api/v1.5/tr.json/translate?key=#{key}&text=#{text}&lang=ru")
   response = JSON.parse(Net::HTTP.get(uri))
   response['text'][0]
+end
+
+def google_translate(text)
+  text = text.gsub(/[^[:ascii:]]/, '')
+  uri = URI("https://translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl=ru&dt=t&q=#{text}")
+  response = JSON.parse(Net::HTTP.get(uri))
+  response[0][0][0]
 end
 
 if ARGV.length < 2
@@ -18,6 +25,7 @@ end
 
 file_to_translate = ARGV[0]
 result_file = ARGV[1]
+service = ARGV[2] || 'google'
 
 doc = File.open(file_to_translate) { |f| Nokogiri::XML(f) }
 paragraphs = doc.css('p')
@@ -29,10 +37,11 @@ iter_to = paragraphs.length - 1
   emphasis_node = Nokogiri::XML::Node.new 'emphasis', doc
   translated_p.add_child emphasis_node
   empty_line = Nokogiri::XML::Node.new 'empty-line', doc
-  emphasis_node.content = translate(paragraphs[i].content)
+  emphasis_node.content = google_translate(paragraphs[i].content)
   paragraphs[i].add_next_sibling(empty_line)
   paragraphs[i].add_next_sibling(translated_p)
   p i * 100 / iter_to.to_f
+  sleep(0.1)
 end
 
 File.write(result_file, doc.to_xml)
